a470b75a386e9c81d6f1771a8f2f4c13131806a2
Joshua <jssmiller94@gmail.com>
2021-06-29 10:43:23 +0100

    opened .zip from spring

---
+++ millu_web_server/server/src/main/java/com/milluWebServer/server/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.milluWebServer.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
