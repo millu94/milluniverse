80e71fc3f83e38292244783e23d9335fb1558255
Joshua <jssmiller94@gmail.com>
2021-07-01 13:46:51 +0100

    hannah helped me with creating a subtotal function

--- millu_web_client/src/components/basket/Basket.jsx
+++ millu_web_client/src/components/basket/Basket.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Container, Typography, Button, Grid } from '@material-ui/core';
 
 import useStyles from './styles';
 
 const Basket = ({ basket }) => {
+
+    // const [subtotal, setSubtotal] = useState([]);
 
     const classes = useStyles();
 
@@ -13,19 +15,29 @@ const Basket = ({ basket }) => {
         <Typography variant="subtitle1">Your basket is empty</Typography>
     )
 
+    const subtotal = function(basket){
+        
+        const Total = basket.reduce((tempTotal, basket) => {
+            return tempTotal + basket.product.price;
+        }, 0)
+        return Total;
+    }
+
+
     const FilledBasket  = () => (
         <>
             <Grid container spacing={3}>
                 {basket.map((item) => (
                     <Grid item xs={12} sm={4} key={item.id}>
                         <div>{item.product.name}</div>
-                        {console.log(item.product.name)}
+                        
                     </Grid>    
                 ))}
             </Grid>
             <div className={classes.cardDetails}>
                 <Typography variant="h4" >
-                    subtotal: {basket.length}
+                    subtotal: £ { isEmpty ? <EmptyBasket/> : subtotal(basket)}
+                    {console.log(subtotal)}
                     <div className={classes.buttons} >
                     <Button className={classes.emptyButton} size="large" type="button" variant="contained" color="secondary">Empty cart</Button>
                     <Button className={classes.checkoutButton} size="large" type="button" variant="contained" color="primary">Checkout</Button>
